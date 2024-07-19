54cf0d0d49f195eb5e185ad492b8b83669014389
Yamato <66829532+louis1706@users.noreply.github.com>
2024-07-19 22:32:03 +0200

    fIx StranglingEventCrash (#103)
    Co-authored-by: IRacle <zornik2006@yandex.ru>

--- EXILED/Exiled.Events/EventArgs/Scp3114/StranglingEventArgs.cs
+++ EXILED/Exiled.Events/EventArgs/Scp3114/StranglingEventArgs.cs
@@ -1,4 +1,4 @@
-﻿// -----------------------------------------------------------------------
+// -----------------------------------------------------------------------
 // <copyright file="StranglingEventArgs.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
@@ -7,69 +7,48 @@
 
 namespace Exiled.Events.EventArgs.Scp3114
 {
-    using Exiled.API.Features;
-    using Exiled.Events.EventArgs.Interfaces;
-
+    using API.Features;
+    using Interfaces;
     using PlayerRoles.PlayableScps.Scp3114;
 
-    using HumanRole = PlayerRoles.HumanRole;
-    using Scp3114Role = API.Features.Roles.Scp3114Role;
+    using Scp3114Role = Exiled.API.Features.Roles.Scp3114Role;
 
     /// <summary>
-    /// Contains all information before strangling a player.
+    ///     Contains all information before SCP-3114 strangles a player.
     /// </summary>
     public class StranglingEventArgs : IScp3114Event, IDeniableEvent
     {
-        private readonly Scp3114Strangle strangle;
-        private Player target = null;
-
         /// <summary>
-        /// Initializes a new instance of the <see cref="StranglingEventArgs"/> class.
+        ///     Initializes a new instance of the <see cref="StranglingEventArgs" /> class.
         /// </summary>
-        /// <param name="strangle">The <see cref="Scp3114Strangle"/> instance.</param>
-        /// <param name="player">The <see cref="API.Features.Player"/> triggering the event.</param>
-        /// <param name="target">The <see cref="API.Features.Player"/> being targeted.</param>
-        public StranglingEventArgs(Scp3114Strangle strangle, Player player, Scp3114Strangle.StrangleTarget target)
+        /// <param name="hub">
+        ///     The <see cref="Player"/> instance which this is being instantiated from.
+        /// </param>
+        /// <param name="target">
+        ///     The <see cref="API.Features.Player"/> being targeted.
+        /// </param>
+        public StranglingEventArgs(ReferenceHub hub, ReferenceHub target)
         {
-            this.strangle = strangle;
-            Scp3114 = player.Role.As<Scp3114Role>();
-            Player = player;
-            Target = Player.Get(target.Target);
-            StrangleTarget = target;
+            Player = Player.Get(hub);
+            Scp3114 = Player.Role.As<Scp3114Role>();
+            Target = Player.Get(target);
         }
 
-        /// <inheritdoc />
+        /// <inheritdoc/>
+        /// <summary>
+        ///     The <see cref="Player"/> who is Scp-3114.
+        /// </summary>
+        public Player Player { get; }
+
+        /// <inheritdoc/>
         public Scp3114Role Scp3114 { get; }
 
-        /// <inheritdoc />
-        public Player Player
-        {
-            get => target;
-            set
-            {
-                target = value;
-
-                if (target == null)
-                {
-                    StrangleTarget = default(Scp3114Strangle.StrangleTarget);
-                    return;
-                }
-
-                StrangleTarget = new(target.ReferenceHub, strangle.GetStranglePosition(target.ReferenceHub.roleManager.CurrentRole as HumanRole), Player.Position);
-            }
-        }
-
         /// <summary>
-        /// Gets the target player.
+        ///     Gets the <see cref="Player"/> being strangled.
         /// </summary>
         public Player Target { get; }
 
-        /// <summary>
-        /// Gets the <see cref="Scp3114Strangle.StrangleTarget"/>. This value is updated when <see cref="Target"/> is changed.
-        /// </summary>
-        public Scp3114Strangle.StrangleTarget? StrangleTarget { get; private set; }
-
-        /// <inheritdoc />
+        /// <inheritdoc/>
         public bool IsAllowed { get; set; } = true;
     }
 }
