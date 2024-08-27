af3da2d92e71d38321f5c29b0df49e63c130431f
ZeroTwo <63092138+NotZer0Two@users.noreply.github.com>
2024-08-27 20:59:04 +0200

    `[EXILED::API]` Adding MoveNetworkIdentityObject (#48)
    * MoveNetworkObject Added
    
    * fix builds
    
    * Fix Builds
    
    ---------
    
    Co-authored-by: Yamato <66829532+louis1706@users.noreply.github.com>

--- EXILED/Exiled.API/Extensions/MirrorExtensions.cs
+++ EXILED/Exiled.API/Extensions/MirrorExtensions.cs
@@ -357,6 +357,24 @@ namespace Exiled.API.Extensions
         }
 
         /// <summary>
+        /// Moves object for the player.
+        /// </summary>
+        /// <param name="player">Target to send.</param>
+        /// <param name="identity">The <see cref="Mirror.NetworkIdentity"/> to move.</param>
+        /// <param name="pos">The position to change.</param>
+        public static void MoveNetworkIdentityObject(this Player player, NetworkIdentity identity, Vector3 pos)
+        {
+            identity.gameObject.transform.position = pos;
+            ObjectDestroyMessage objectDestroyMessage = new()
+            {
+                netId = identity.netId,
+            };
+
+            player.Connection.Send(objectDestroyMessage, 0);
+            SendSpawnMessageMethodInfo?.Invoke(null, new object[] { identity, player.Connection });
+        }
+
+        /// <summary>
         /// Sends to the player a Fake Change Scene.
         /// </summary>
         /// <param name="player">The player to send the Scene.</param>
@@ -401,6 +419,26 @@ namespace Exiled.API.Extensions
 
             player.Connection.Send(objectDestroyMessage, 0);
             SendSpawnMessageMethodInfo?.Invoke(null, new object[] { identity, player.Connection });
+        }
+
+        /// <summary>
+        /// Moves object for all the players.
+        /// </summary>
+        /// <param name="identity">The <see cref="NetworkIdentity"/> to move.</param>
+        /// <param name="pos">The position to change.</param>
+        public static void MoveNetworkIdentityObject(this NetworkIdentity identity, Vector3 pos)
+        {
+            identity.gameObject.transform.position = pos;
+            ObjectDestroyMessage objectDestroyMessage = new()
+            {
+                netId = identity.netId,
+            };
+
+            foreach (Player ply in Player.List)
+            {
+                ply.Connection.Send(objectDestroyMessage, 0);
+                SendSpawnMessageMethodInfo?.Invoke(null, new object[] { identity, ply.Connection });
+            }
         }
 
         /// <summary>
