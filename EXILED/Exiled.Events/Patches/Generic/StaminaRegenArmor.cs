5aa87548bb89c3b7a34f0a047947d64fba579154
ZeroTwo <63092138+NotZer0Two@users.noreply.github.com>
2024-08-27 16:39:24 +0200

    `[EXILED::API]` Adding StaminaRegenMultiplier (BodyArmor) (#71)
    * First push
    
    * using removed
    
    * Added to the pickup
    
    * Forgot to add to Armor (not pickup)
    
    * Fix Build error
    
    * Simplification

---
+++ EXILED/Exiled.Events/Patches/Generic/StaminaRegenArmor.cs
@@ -0,0 +1,30 @@
+// -----------------------------------------------------------------------
+// <copyright file="StaminaRegenArmor.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Patches.Generic
+{
+    using Exiled.API.Features;
+    using Exiled.API.Features.Items;
+#pragma warning disable SA1313
+
+    using HarmonyLib;
+    using InventorySystem.Items.Armor;
+
+    /// <summary>
+    /// Patches <see cref="BodyArmor.StaminaRegenMultiplier"/>.
+    /// Implements <see cref="API.Features.Items.Armor.StaminaRegenMultiplier"/>.
+    /// </summary>
+    [HarmonyPatch(typeof(BodyArmor), nameof(BodyArmor.StaminaRegenMultiplier), MethodType.Getter)]
+    internal class StaminaRegenArmor
+    {
+        private static void Postfix(BodyArmor __instance, ref float __result)
+        {
+            if(Item.Get(__instance) is Armor armor)
+                __result *= armor.StaminaRegenMultiplier;
+        }
+    }
+}
