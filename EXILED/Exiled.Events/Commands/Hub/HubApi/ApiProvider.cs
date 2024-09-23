c91a606a3652e6e9e6cab6c479ac6231b8a292b4
Panikorovskii Vladislav <68610520+bladuk@users.noreply.github.com>
2024-09-23 21:43:45 +0300

    `[EXILED::Events]` Introduce `hub install` command (#101)
    * feat: introduce plugin installation command
    
    * fix: fix build errors
    
    wtf was this actually

---
+++ EXILED/Exiled.Events/Commands/Hub/HubApi/ApiProvider.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ApiProvider.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Commands.Hub.HubApi
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using Exiled.Events.Commands.Hub.HubApi.Models;
+
+    using Utf8Json;
+
+    /// <summary>
+    /// An API bridge to EXILED Hub.
+    /// </summary>
+    public static class ApiProvider
+    {
+        /// <summary>
+        /// The API endpoint to get the plugin installation data.
+        /// </summary>
+        private const string InstallApiEndpoint = "https://hub.exiled-team.net/api/install?name=";
+
+        /// <summary>
+        /// Gets installation data of the plugin by name.
+        /// </summary>
+        /// <param name="pluginName">The name of plugin.</param>
+        /// <param name="client">The <see cref="HttpClient"/>.</param>
+        /// <returns>A <see cref="HubPlugin"/> instance containing installation data.</returns>
+        public static async Task<HubPlugin?> GetInstallationData(string pluginName, HttpClient client)
+        {
+            string url = InstallApiEndpoint + pluginName;
+            using HttpResponseMessage response = await client.GetAsync(url).ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using Stream stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
+                return JsonSerializer.Deserialize<HubPlugin>(stream);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Creates a HTTP client for EXILED Hub API.
+        /// </summary>
+        /// <returns>Created HTTP client.</returns>
+        internal static HttpClient CreateClient()
+        {
+            HttpClient client = new();
+
+            client.Timeout = TimeSpan.FromSeconds(460);
+            client.DefaultRequestHeaders.Add("User-Agent", $"Exiled.Events (https://github.com/ExMod-Team/EXILED, {Assembly.GetExecutingAssembly().GetName().Version.ToString(3)})");
+
+            return client;
+        }
+    }
+}
---
+++ EXILED/Exiled.Events/Commands/Hub/Install.cs
@@ -0,0 +1,116 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Install.cs" company="Exiled Team">
+// Copyright (c) Exiled Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.Events.Commands.Hub
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+
+    using CommandSystem;
+
+    using Exiled.API.Features;
+    using Exiled.Events.Commands.Hub.HubApi.Models;
+    using Exiled.Loader;
+    using Exiled.Loader.GHApi;
+    using Exiled.Loader.GHApi.Models;
+    using Exiled.Loader.GHApi.Settings;
+    using Exiled.Permissions.Extensions;
+
+    using RemoteAdmin;
+
+    /// <summary>
+    /// The command to install a plugin from EXILED Hub.
+    /// </summary>
+    public class Install : ICommand, IUsageProvider
+    {
+        /// <summary>
+        /// Gets static instance of the <see cref="Install"/> command.
+        /// </summary>
+        public static Install Instance { get; } = new();
+
+        /// <inheritdoc/>
+        public string Command { get; } = "install";
+
+        /// <inheritdoc/>
+        public string[] Aliases { get; } = { "i" };
+
+        /// <inheritdoc/>
+        public string[] Usage { get; } = { "Plugin name", "Release tag (optional)" };
+
+        /// <inheritdoc/>
+        public string Description { get; } = "Installs a plugin from EXILED Hub.";
+
+        /// <inheritdoc/>
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            const string permission = "hub.install";
+
+            if (!sender.CheckPermission(permission) && sender is PlayerCommandSender playerSender && !playerSender.FullPermissions)
+            {
+                response = $"You don't have permissions to install the plugins. Required permission node: \"{permission}\".";
+                return false;
+            }
+
+            if (arguments.Count == 0)
+            {
+                response = "Missing arguments! Usage: hub install <plugin name> (release tag)";
+                return false;
+            }
+
+            using HttpClient client = HubApi.ApiProvider.CreateClient();
+
+            HubPlugin? pluginData = HubApi.ApiProvider.GetInstallationData(arguments.At(0), client).GetAwaiter().GetResult();
+
+            if (pluginData == null)
+            {
+                response = "An error has occurred while fetching the plugin data. Please check if the plugin name is correct and try again.";
+                return false;
+            }
+
+            Release[] pluginReleases = client.GetReleases(pluginData.Value.RepositoryId, new GetReleasesSettings(50, 1)).GetAwaiter().GetResult();
+            Release releaseToDownload = pluginReleases[0];
+
+            if (arguments.Count > 1)
+            {
+                Release foundRelease = pluginReleases.FirstOrDefault(x => x.TagName == arguments.At(1));
+
+                if (foundRelease.Id == 0)
+                {
+                    response = "Release with the provided tag not found.";
+                    return false;
+                }
+
+                releaseToDownload = foundRelease;
+            }
+
+            Log.Info($"Downloading release \"{releaseToDownload.TagName}\". Found {releaseToDownload.Assets.Length} asset(s) to download.");
+
+            foreach (ReleaseAsset asset in releaseToDownload.Assets)
+            {
+                Log.Info($"Downloading asset {asset.Name}. Asset size: {Math.Round(asset.Size / 1000f, 2)} KB.");
+                using HttpResponseMessage assetResponse = client.GetAsync(asset.BrowserDownloadUrl).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                string pluginPath = Path.Combine(Paths.Plugins, asset.Name);
+
+                if (File.Exists(pluginPath) && Environment.OSVersion.Platform == PlatformID.Unix)
+                    LinuxPermission.SetFileUserAndGroupReadWriteExecutePermissions(pluginPath);
+
+                using Stream stream = assetResponse.Content.ReadAsStreamAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+                using FileStream fileStream = new(pluginPath, FileMode.Create, FileAccess.Write, FileShare.None);
+                stream.CopyToAsync(fileStream).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                if (Environment.OSVersion.Platform == PlatformID.Unix)
+                    LinuxPermission.SetFileUserAndGroupReadWriteExecutePermissions(pluginPath);
+            }
+
+            response = $"{arguments.At(0)} has been successfully installed.";
+            return true;
+        }
+    }
+}
