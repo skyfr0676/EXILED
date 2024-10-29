1681b8a089af324ae07291cd4a6074cde9d68bef
Yamato <louismonneyron5@yahoo.com>
2024-10-29 12:49:47 +0100

    fix IL Error (yamato skill issue)

--- EXILED/Exiled.Events/Patches/Events/Server/RoundEnd.cs
+++ EXILED/Exiled.Events/Patches/Events/Server/RoundEnd.cs
@@ -137,7 +137,7 @@ namespace Exiled.Events.Patches.Events.Server
             index = newInstructions.FindIndex(x => x.opcode == OpCodes.Stfld && x.operand == (object)Field(typeof(SumInfo_ClassList), nameof(SumInfo_ClassList.warhead_kills))) + offset;
             newInstructions.InsertRange(index, new CodeInstruction[]
             {
-                new(OpCodes.Ldflda, Field(PrivateType, NewList)),
+                new(OpCodes.Ldfld, Field(PrivateType, NewList)),
                 new(OpCodes.Call, PropertySetter(typeof(Round), nameof(Round.LastClassList))),
             });
 
