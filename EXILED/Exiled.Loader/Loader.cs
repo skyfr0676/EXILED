f0899299b42e95ed87a3694996d2692c15d10d59
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-11-30 21:26:16 +0100

    Delete Exiled.API (#280)

--- EXILED/Exiled.Loader/Loader.cs
+++ EXILED/Exiled.Loader/Loader.cs
@@ -121,6 +121,7 @@ namespace Exiled.Loader
         public static void LoadPlugins()
         {
             File.Delete(Path.Combine(Paths.Plugins, "Exiled.Updater.dll"));
+            File.Delete(Path.Combine(Paths.Dependencies, "Exiled.API.dll"));
 
             foreach (string assemblyPath in Directory.GetFiles(Paths.Plugins, "*.dll"))
             {
