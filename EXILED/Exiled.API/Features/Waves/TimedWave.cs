e84b3764870ff51647fe3401832eb49f6d2b7c7a
Jay <me@jxtq.moe>
2024-12-03 15:07:54 +0000

    feat: timed waves (#286)
    * feat: timed waves
    
    * feat: get all wave timers
    
    Not specific to factions and stuff
    
    * feat: name and mini wave checks
    
    * feat: unpause on timer
    
    * feat: get wave by type
    
    * fix: small bugs
    
    ---------
    
    Co-authored-by: Misaka-ZeroTwo <45165615+Misaka-ZeroTwo@users.noreply.github.com>

---
+++ EXILED/Exiled.API/Features/Waves/TimedWave.cs
@@ -0,0 +1,188 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TimedWave.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Features.Waves
+{
+    using System.Collections.Generic;
+
+    using System.Linq;
+
+    using PlayerRoles;
+
+    using Respawning;
+
+    using Respawning.Waves;
+
+    /// <summary>
+    /// Represents a timed wave.
+    /// </summary>
+    public class TimedWave
+    {
+        private readonly TimeBasedWave timedWave;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TimedWave"/> class.
+        /// </summary>
+        /// <param name="wave">
+        /// The <see cref="TimeBasedWave"/> that this class should be based off of.
+        /// </param>
+        public TimedWave(TimeBasedWave wave)
+        {
+            timedWave = wave;
+        }
+
+        /// <summary>
+        /// Gets the name of the wave timer.
+        /// </summary>
+        public string Name => timedWave.GetType().Name;
+
+        /// <summary>
+        /// Gets a value indicating whether the wave is a mini wave.
+        /// </summary>
+        public bool IsMiniWave => timedWave is IMiniWave;
+
+        /// <summary>
+        /// Gets the wave timer instance.
+        /// </summary>
+        public WaveTimer Timer => new(timedWave.Timer);
+
+        /// <summary>
+        /// Gets the faction of this wave.
+        /// </summary>
+        public Faction Faction => timedWave.TargetFaction;
+
+        /// <summary>
+        /// Gets the team of this wave.
+        /// </summary>
+        public SpawnableTeamType Team => timedWave.TargetFaction.GetSpawnableTeam();
+
+        /// <summary>
+        /// Gets the maximum amount of people that can spawn in this wave.
+        /// </summary>
+        public int MaxAmount => timedWave.MaxWaveSize;
+
+        /// <summary>
+        /// Get the timed waves for the specified faction.
+        /// </summary>
+        /// <param name="faction">
+        /// The faction to get the waves for.
+        /// </param>
+        /// <param name="waves">
+        /// The waves if found.
+        /// </param>
+        /// <returns>
+        /// A value indicating whether the wave were found.
+        /// </returns>
+        public static bool TryGetTimedWaves(Faction faction, out List<TimedWave> waves)
+        {
+            List<SpawnableWaveBase> spawnableWaveBases = WaveManager.Waves.Where(w => w is TimeBasedWave wave && wave.TargetFaction == faction).ToList();
+            if(!spawnableWaveBases.Any())
+            {
+                waves = null;
+                return false;
+            }
+
+            waves = spawnableWaveBases.Select(w => new TimedWave((TimeBasedWave)w)).ToList();
+            return true;
+        }
+
+        /// <summary>
+        /// Get the timed wave for the specified team.
+        /// </summary>
+        /// <param name="team">
+        /// The team to get the wave for.
+        /// </param>
+        /// <param name="waves">
+        /// The waves if found.
+        /// </param>
+        /// <returns>
+        /// A value indicating whether the wave were found.
+        /// </returns>
+        public static bool TryGetTimedWaves(SpawnableTeamType team, out List<TimedWave> waves)
+        {
+            if (team == SpawnableTeamType.None)
+            {
+                waves = null;
+                return false;
+            }
+
+            Faction faction = team == SpawnableTeamType.NineTailedFox ? Faction.FoundationStaff : Faction.FoundationEnemy;
+
+            return TryGetTimedWaves(faction, out waves);
+        }
+
+        /// <summary>
+        /// Get the timed wave for the specified type.
+        /// </summary>
+        /// <param name="wave">
+        /// The wave type to get.
+        /// </param>
+        /// <typeparam name="T">
+        /// The type of wave to get. Must be a <see cref="TimeBasedWave"/>. I.e. <see cref="NtfSpawnWave"/> or <see cref="NtfMiniWave"/>.
+        /// </typeparam>
+        /// <returns>
+        /// A value indicating whether the wave was found.
+        /// </returns>
+        public static bool TryGetTimedWave<T>(out TimedWave wave)
+            where T : TimeBasedWave
+        {
+            foreach (SpawnableWaveBase waveBase in WaveManager.Waves)
+            {
+                if (waveBase is not TimeBasedWave timeWave || timeWave.GetType() != typeof(T))
+                    continue;
+
+                wave = new(timeWave);
+                return true;
+            }
+
+            wave = null;
+            return false;
+        }
+
+        /// <summary>
+        /// Get all timed waves.
+        /// </summary>
+        /// <returns>
+        /// A list of all timed waves.
+        /// </returns>
+        public static List<TimedWave> GetTimedWaves()
+        {
+            List<TimedWave> waves = new();
+            foreach (SpawnableWaveBase wave in WaveManager.Waves)
+            {
+                if (wave is TimeBasedWave timeBasedWave)
+                {
+                    waves.Add(new (timeBasedWave));
+                }
+            }
+
+            return waves;
+        }
+
+        /// <summary>
+        /// Destroys this wave.
+        /// </summary>
+        public void Destroy()
+        {
+            timedWave.Destroy();
+        }
+
+        /// <summary>
+        /// Populates this wave with the specified amount of roles.
+        /// </summary>
+        /// <param name="queue">
+        /// The queue to populate.
+        /// </param>
+        /// <param name="amount">
+        /// The amount of people to populate.
+        /// </param>
+        public void PopulateQueue(Queue<RoleTypeId> queue, int amount)
+        {
+            timedWave.PopulateQueue(queue, amount);
+        }
+    }
+}
---
+++ EXILED/Exiled.API/Features/Waves/WaveTimer.cs
@@ -0,0 +1,217 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="WaveTimer.cs" company="ExMod Team">
+// Copyright (c) ExMod Team. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Exiled.API.Features.Waves
+{
+    using System;
+
+    using System.Collections.Generic;
+
+    using System.Linq;
+
+    using PlayerRoles;
+
+    using Respawning;
+
+    using Respawning.Waves;
+
+    /// <summary>
+    /// Represents a wave timer.
+    /// </summary>
+    public class WaveTimer
+    {
+        /// <summary>
+        /// Get the native <see cref="Respawning.Waves.WaveTimer"/>.
+        /// </summary>
+        private readonly Respawning.Waves.WaveTimer waveTimer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WaveTimer"/> class.
+        /// </summary>
+        /// <param name="wave">The <see cref="Respawning.Waves.WaveTimer"/> that this class should be based off of.</param>
+        public WaveTimer(Respawning.Waves.WaveTimer wave)
+        {
+            waveTimer = wave;
+        }
+
+        /// <summary>
+        /// Gets the name of the wave timer.
+        /// </summary>
+        public string Name => waveTimer._wave.GetType().Name;
+
+        /// <summary>
+        /// Gets a value indicating whether the wave is a mini wave.
+        /// </summary>
+        public bool IsMiniWave => waveTimer._wave is IMiniWave;
+
+        /// <summary>
+        /// Gets the amount of time left before the wave spawns.
+        /// </summary>
+        public TimeSpan TimeLeft => TimeSpan.FromSeconds(waveTimer.TimeLeft);
+
+        /// <summary>
+        /// Gets the amount of time passed since the last wave spawned.
+        /// </summary>
+        public TimeSpan TimePassed => TimeSpan.FromSeconds(waveTimer.TimePassed);
+
+        /// <summary>
+        /// Gets the amount of time left before this wave unpause.
+        /// </summary>
+        public TimeSpan PauseTimeLeft => TimeSpan.FromSeconds(waveTimer.PauseTimeLeft);
+
+        /// <summary>
+        /// Gets the amount of time this wave has been paused for.
+        /// </summary>
+        public TimeSpan PausedFor => TimeSpan.FromSeconds(waveTimer._pauseTimer);
+
+        /// <summary>
+        /// Gets a value indicating whether this wave is paused.
+        /// </summary>
+        public bool IsPaused => waveTimer.IsPaused;
+
+        /// <summary>
+        /// Gets a value indicating whether this wave is ready to spawn.
+        /// </summary>
+        public bool IsReady => waveTimer.IsReadyToSpawn;
+
+        /// <summary>
+        /// Gets a value indicating whether this wave is out of respawns.
+        /// </summary>
+        public bool IsRespawnable => !waveTimer.IsOutOfRespawns;
+
+        /// <summary>
+        /// Gets the default amount of time between a respawn of this wave.
+        /// </summary>
+        public float DefaultSpawnInterval => waveTimer.DefaultSpawnInterval;
+
+        /// <summary>
+        /// Gets the actual amount of time between a respawn of this wave.
+        /// </summary>
+        public float SpawnInterval => waveTimer.SpawnIntervalSeconds;
+
+        /// <summary>
+        /// Get the wave timers for the specified faction.
+        /// </summary>
+        /// <param name="faction">The faction.</param>
+        /// <param name="waves">The waves, if any.</param>
+        /// <returns>A bool indicating if waves were found.</returns>
+        public static bool TryGetWaveTimers(Faction faction, out List<WaveTimer> waves)
+        {
+            if (!TimedWave.TryGetTimedWaves(faction, out List<TimedWave> timedWaves))
+            {
+                waves = null;
+                return false;
+            }
+
+            waves = timedWaves.Select(wave => wave.Timer).ToList();
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the wave timers for the specified team.
+        /// </summary>
+        /// <param name="team">The team.</param>
+        /// <param name="waves">The waves, if any.</param>
+        /// <returns>A bool indicating if waves were found.</returns>
+        public static bool TryGetWaveTimers(SpawnableTeamType team, out List<WaveTimer> waves)
+        {
+            if (!TimedWave.TryGetTimedWaves(team, out List<TimedWave> timedWaves))
+            {
+                waves = null;
+                return false;
+            }
+
+            waves = timedWaves.Select(wave => wave.Timer).ToList();
+            return true;
+        }
+
+        /// <summary>
+        /// Gets all wave timers.
+        /// </summary>
+        /// <returns>A list of all wave timers.</returns>
+        public static List<WaveTimer> GetWaveTimers()
+        {
+            return TimedWave.GetTimedWaves().Select(l => l.Timer).ToList();
+        }
+
+        /// <summary>
+        /// Destroys this wave timer.
+        /// </summary>
+        public void Destroy()
+        {
+            waveTimer.Destroy();
+        }
+
+        /// <summary>
+        /// Pauses this wave timer.
+        /// </summary>
+        /// <param name="seconds">
+        /// The amount of time to pause this wave timer for.
+        /// </param>
+        public void Pause(float seconds)
+        {
+            waveTimer.Pause(seconds);
+        }
+
+        /// <summary>
+        /// Unpauses this wave timer.
+        /// </summary>
+        public void Unpause()
+        {
+            waveTimer.Pause(0);
+        }
+
+        /// <summary>
+        /// Resets this wave timer.
+        /// </summary>
+        /// <param name="resetInterval">
+        /// A value indicating whether the <see cref="SpawnInterval"/> should be reset.
+        /// </param>
+        public void Reset(bool resetInterval = true)
+        {
+            waveTimer.Reset(resetInterval);
+        }
+
+        /// <summary>
+        /// Update the timer.
+        /// </summary>
+        public void Update()
+        {
+            waveTimer.Update();
+        }
+
+        /// <summary>
+        /// Add time to the wave timer.
+        /// </summary>
+        /// <param name="seconds">
+        /// The amount of time to add in seconds.
+        /// </param>
+        public void AddTime(float seconds)
+        {
+            waveTimer.AddTime(seconds);
+        }
+
+        /// <summary>
+        /// Set the amount of time before the wave spawns.
+        /// </summary>
+        /// <param name="time">
+        /// The amount of time before the wave spawns.
+        /// </param>
+        public void SetTime(TimeSpan time) => SetTime((float)time.TotalSeconds);
+
+        /// <summary>
+        /// Set the amount of time before the wave spawns.
+        /// </summary>
+        /// <param name="seconds">
+        /// The amount of time before the wave spawns, in seconds.
+        /// </param>
+        public void SetTime(float seconds)
+        {
+            waveTimer.SetTime(seconds);
+        }
+    }
+}
