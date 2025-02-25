1f28777f2ef4e9abc0cc3a843b77a2b9aa757136
TtroubleTT <121741230+TtroubleTT@users.noreply.github.com>
2025-02-25 04:08:39 -0800

    feat: Have NPC In Player List (#445)
    npc are in player list

--- EXILED/Exiled.API/Features/Player.cs
+++ EXILED/Exiled.API/Features/Player.cs
@@ -138,7 +138,7 @@ namespace Exiled.API.Features
         /// <summary>
         /// Gets a list of all <see cref="Player"/>'s on the server.
         /// </summary>
-        public static IReadOnlyCollection<Player> List => Dictionary.Values.Where(x => !x.IsNPC).ToList();
+        public static IReadOnlyCollection<Player> List => Dictionary.Values.ToList();
 
         /// <summary>
         /// Gets a <see cref="Dictionary{TKey, TValue}"/> containing cached <see cref="Player"/> and their user ids.
