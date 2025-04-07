0155a58bac371fe4c4de4e668e95f55601d4cd60
Mike <146554836+MikeSus1@users.noreply.github.com>
2025-04-07 23:08:58 +0200

    feat: Add new event MicroHIDOpeningDoorEventArgs (#466)
    * addevent
    
    * force rebuild
    
    * now should be fixed
    
    * docs fix woops
    
    * Added the door property
    
    * fix build using
    
    * i swear if build fails
    
    * remove setter
    
    * Gets
    
    * ev.IsAllowed changed
    
    * functional Prefix
    
    * Remove Prefix code
    
    ---------
    
    Co-authored-by: Yamato <louismonneyron5@yahoo.com>

--- EXILED/Exiled.Events/Handlers/Player.cs
+++ EXILED/Exiled.Events/Handlers/Player.cs
@@ -584,6 +584,11 @@ namespace Exiled.Events.Handlers
         public static Event<ChangingDisruptorModeEventArgs> ChangingDisruptorMode { get; set; } = new();
 
         /// <summary>
+        /// Invoked before the micro HID opens a door.
+        /// </summary>
+        public static Event<MicroHIDOpeningDoorEventArgs> MicroHIDOpeningDoor { get; set; } = new();
+
+        /// <summary>
         /// Invoked before player interacts with coffee cup.
         /// </summary>
         [Obsolete("Never available (for now).")]
@@ -1259,6 +1264,12 @@ namespace Exiled.Events.Handlers
         /// </summary>
         /// <param name="ev">The <see cref="ChangingDisruptorModeEventArgs"/> instance.</param>
         public static void OnChangingDisruptorMode(ChangingDisruptorModeEventArgs ev) => ChangingDisruptorMode.InvokeSafely(ev);
+
+        /// <summary>
+        /// Called before the micro HID opens a door.
+        /// </summary>
+        /// <param name="ev">The <see cref="ChangingDisruptorModeEventArgs"/> instance.</param>
+        public static void OnMicroHIDOpeningDoor(MicroHIDOpeningDoorEventArgs ev) => MicroHIDOpeningDoor.InvokeSafely(ev);
 
         /// <summary>
         /// Called before player interacts with coffee cup.
